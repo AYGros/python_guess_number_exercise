e4d461fb1c00daecefca411497e6eafd930e3d63
YukiGros <73797342+AYGros@users.noreply.github.com>
2021-09-08 22:46:38 +0000

    simple version of game with just while loop, no functions

--- main.py
+++ main.py
@@ -8,5 +8,37 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 from art import logo
+import random
 
 print(logo)
+print("This is a number guessing game.\nI am thinking of a number between 1 and 100 and you have to guess it.")
+
+level = input("Choose a difficulty level by typing 'easy' or 'hard': ")
+if level == "easy":
+  rounds = 10
+else:
+  rounds = 5
+
+number = random.randint(1,100)
+print(f"psssst, the secret number is {number}")
+solved = False
+
+# def decrease_rounds():
+#   return rounds - 1
+
+while not rounds == 0 and solved == False :
+  guess = int(input("Make a guess: "))
+  if guess == number:
+    print(f"You guessed it! I thought of {number}")
+    solved = True
+  elif guess > number:
+    print("Too high.")
+    rounds -= 1
+    print(f"You have {rounds} guesses left")
+  else:
+    print("Too low.")
+    rounds -= 1
+    print(f"You have {rounds} guesses left")
+
+if rounds == 0:
+  print(f"Sorry, you took too long. The secret number was {number}")
